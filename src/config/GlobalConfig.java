e87305f8d0bc20683439ad98d85c80528f0cbca3
5731111121-AI <kame_1996@hotmail.com>
2015-12-07 01:13:45 +0700

    add font.

--- src/config/GlobalConfig.java
+++ src/config/GlobalConfig.java
@@ -1,5 +1,7 @@
 package config;
 
+import java.awt.Font;
+
 public class GlobalConfig {
 
 	public static final String GAME_NAME = "ASTERA STRIKE";
@@ -8,6 +10,8 @@ public class GlobalConfig {
 	public static final int SCREEN_HEIGHT = 768;
 	public static final int SCREEN_WIDTH_CENTER = SCREEN_WIDTH / 2;
 	public static final int SCREEN_HEIGHT_CENTER = SCREEN_HEIGHT / 2;
+	public static final Font mainFont = new Font("VanadineRg", Font.PLAIN, 40);
+	public static final Font subFont = new Font("VenusRisingRg", Font.PLAIN, 40);
 	//game configuration
 	public static final int DEFAULT_SPEEDX = 50;
 	public static final int DEFAULT_SPEEDY = 50;
